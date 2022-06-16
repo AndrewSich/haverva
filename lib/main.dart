f61b64882dc53b294027bdc03b95b3894ef48a46
FlutLab.io <>
2022-06-16 11:02:49 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'utils/theme.dart';
-import 'splash_screen.dart';
+//import 'splash_screen.dart';
 
 void main() => runApp(HaverVA());
 
@@ -12,10 +12,18 @@ class HaverVA extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: LightThemeData(context),
       darkTheme: DarkThemeData(context),
-      // theme: ThemeData(
-      //   primarySwatch: kPrimaryColor,
-      // ),
       home: Splash_Screen(),
+    );
+  }
+}
+
+class Splash_Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('by Andrew Setyawan'),
+      ),
     );
   }
 }
